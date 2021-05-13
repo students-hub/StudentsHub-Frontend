9e0332cb2fb79bed4bf0dee8864ca710d145cf3f
Sh-Zh-7 <2431297348@qq.com>
2021-05-13 12:01:52 +0800

    refactor(login): build app's skeleton.

---
+++ login/src/app/core/core.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { ShareModule } from '../share/share.module';
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ShareModule,
+  ],
+  exports: [
+    ShareModule,
+    AppRoutingModule,
+  ],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+})
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule can only be introduced by AppModule.');
+    }
+  }
+}
