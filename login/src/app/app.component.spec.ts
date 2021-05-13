6f08a825f58d9a84a165551c0f6744cd759deaa7
Sh-Zh-7 <2431297348@qq.com>
2021-05-13 11:08:24 +0800

    refactor: move login part to separate folder.

--- login/src/app/app.component.spec.ts
+++ login/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StudentsHub-Frontend'`, () => {
+  it(`should have as title 'StudentsHub-Login'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('StudentsHub-Frontend');
+    expect(app.title).toEqual('StudentsHub-Login');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('StudentsHub-Frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('StudentsHub-Login app is running!');
   });
 });
