b88913928f762a9413b89a5db7a5bfd04c460b9c
Sh-Zh-7 <2431297348@qq.com>
2021-06-01 21:50:40 +0800

    feat(storage): add element-ui dependency.

--- storage/src/main.js
+++ storage/src/main.js
@@ -2,6 +2,10 @@ import './public-path';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'normalize.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false;
 
