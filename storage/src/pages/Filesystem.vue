f73500173d2fa104c9f40066e9cb9890553e0b5b
Sh-Zh-7 <2431297348@qq.com>
2021-06-07 14:33:28 +0800

    feat(storage): add simple upload dialog with element.

--- storage/src/pages/Filesystem.vue
+++ storage/src/pages/Filesystem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="filesystem">
+  <div id="filesystem" v-contextMenu="menuObj">
     <el-breadcrumb separator="/">
       <el-breadcrumb-item 
         :to="dirs.length !== 0? {}: undefined"
@@ -33,10 +33,35 @@
         />
       </div>
     </div>
+
+    <el-dialog
+      title="上传文件" 
+      :width="'35%'"
+      :visible.sync="uploadFormVisiable"
+    >
+      <el-upload
+        drag
+        class="upload"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="uploadFormVisiable = false">取 消</el-button>
+        <el-button type="primary" @click="uploadFormVisiable = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <style scoped>
+  #filesystem {
+    width: 100%;
+    height: 100%;
+  }
+
   .main {
     padding: 0;
 
@@ -49,6 +74,11 @@
     margin: 30px;
     margin-right: 75px;
   }
+
+  .upload {
+    width: 350px;
+    margin: 0 auto;
+  }
 </style>
 
 <script>
@@ -56,7 +86,7 @@ import File from '../components/File.vue';
 import Folder from '../components/Folder.vue';
 
 import { list2Path } from '../utils/array';
-import { setFileList } from '../services/request';
+import { setFileList } from '../services/fs';
 
 export default {
   props: {
@@ -68,7 +98,35 @@ export default {
   data: () => ({
     dirs: [],
     files: [],
+    uploadFormVisiable: false,
   }),
+  computed: {
+    menuObj() {
+      return [{
+        title: '上传文件',
+        handler: () => {
+          this.uploadFormVisiable = true;
+        },
+      }, {
+        title: '新建文件夹',
+        handler: () => {
+          this.$prompt('请输入文件夹名', '新建文件夹', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+          }).then(({ value }) => {
+            this.$message({
+              type: 'success',
+              message: "新建文件夹成功"
+            });
+            console.log(value);
+          }).catch(() => {});
+        },
+      }, {
+        title: '刷新',
+        handler: () => console.log('刷新'),
+      }]
+    }
+  },
   watch: {
     rootDir: function() {
       this.dirs = [];
